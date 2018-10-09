96ddaa4785ded2155c1eafcabe0aaf27e10ffdad
Sigurd Midttun <sigurd.midttun@nav.no>
2018-10-09 09:49:32 +0200

    MMA-829 Endret på config og pom'er

--- app/src/main/java/no/nav/saf/ApplicationConfig.java
+++ app/src/main/java/no/nav/saf/ApplicationConfig.java
@@ -3,10 +3,12 @@ package no.nav.saf;
 import io.micrometer.core.instrument.MeterRegistry;
 import no.nav.saf.metrics.DokTimedAspect;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @EnableAspectJAutoProxy
+@ComponentScan
 @Configuration
 public class ApplicationConfig {
 
