91bc78134e5c13a5b39a9a63492c89ee37bfdc33
Stian Låte <stian.late@nav.no>
2018-10-16 10:40:19 +0200

    MMA-869 delte exceptions til funksjonelle og tekniske

--- core-acl/src/main/java/no/nav/saf/context/gsak/hentgsaksaker/GsakConsumer.java
+++ core-acl/src/main/java/no/nav/saf/context/gsak/hentgsaksaker/GsakConsumer.java
@@ -1,7 +1,8 @@
 package no.nav.saf.context.gsak.hentgsaksaker;
 
 import lombok.extern.slf4j.Slf4j;
-import no.nav.saf.context.Exceptions.SafTechnicalException;
+import no.nav.saf.context.exceptions.SafFunctionalException;
+import no.nav.saf.context.exceptions.SafTechnicalException;
 import no.nav.saf.context.gsak.domain.GsakSakerTo;
 import no.nav.saf.fasit.ServiceuserAlias;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,8 +12,6 @@ import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.UUID;
-
 @Slf4j
 @Component
 class GsakConsumer {
@@ -20,13 +19,11 @@ class GsakConsumer {
 	private static final int TIMEOUT = 30_000;
 	private final RestTemplate restTemplate;
 	private final String gsakApiUrl;
-	private final String xCorrelationId;
 
 	public GsakConsumer(RestTemplateBuilder restTemplateBuilder,
 						@Value("${sak.saker.url}") String gsakApiUrl,
 						ServiceuserAlias serviceuserAlias) {
 		this.gsakApiUrl = gsakApiUrl;
-		this.xCorrelationId = UUID.randomUUID().toString(); //TODO bruker en random id for øyeblikket
 		this.restTemplate = restTemplateBuilder
 				.setReadTimeout(TIMEOUT)
 				.setConnectTimeout(TIMEOUT)
@@ -37,12 +34,12 @@ class GsakConsumer {
 	public GsakSakerTo getGsakSaker(final String saksId) {
 		try {
 			return restTemplate.getForObject(this.gsakApiUrl + "/" + saksId, GsakSakerTo.class);
-		} catch (HttpClientErrorException e) {
-			throw new SafTechnicalException(String.format("getGsaksaker feilet pa clientside med statusKode=%s. Feilmelding=%s", e
+		} catch (HttpServerErrorException e) {
+			throw new SafTechnicalException(String.format("getGsaksaker feilet teknisk med statusKode=%s. Feilmelding=%s", e
 					.getStatusCode(), e
 					.getResponseBodyAsString()), e, e.getStatusCode());
-		} catch (HttpServerErrorException e) {
-			throw new SafTechnicalException(String.format("getGsaksaker feilet pa serverside med statusKode=%s. Feilmelding=%s", e
+		} catch (HttpClientErrorException e) {
+			throw new SafFunctionalException(String.format("getGsaksaker feilet funksjonelt med statusKode=%s. Feilmelding=%s", e
 					.getStatusCode(), e
 					.getResponseBodyAsString()), e, e.getStatusCode());
 		}
