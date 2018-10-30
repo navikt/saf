0c4b7be67204ce3d84b6556cee101d10c1c87d19
Stian Låte <stian.late@nav.no>
2018-10-30 14:02:15 +0100

    MMA-1120 La til AbacLogger

--- core-acl/src/main/java/no/nav/saf/tilgangskontroll/AbacLogger.java
+++ core-acl/src/main/java/no/nav/saf/tilgangskontroll/AbacLogger.java
@@ -1,7 +1,14 @@
 package no.nav.saf.tilgangskontroll;
 
+import static org.apache.commons.lang3.StringUtils.substringAfterLast;
+
 import lombok.extern.slf4j.Slf4j;
+import no.nav.saf.tilgangskontroll.abac.dto.request.XacmlRequest;
+import no.nav.saf.tilgangskontroll.abac.dto.response.XacmlResponse;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @author Joakim Bjørnstad, Jbit AS
@@ -9,7 +16,45 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j
 public class AbacLogger {
-	public void logDeny() {
-		// TODO MMA-1120 - Implementer test også!
+	// TODO MMA-1120 - Implementer test også!
+	private static final String SEPARATOR = ".";
+
+	public void logAbacDeny(final XacmlRequest xacmlRequest, final XacmlResponse xacmlResponse, final Map<String, String> resources) {
+		log.warn("{}{}; {}", mapRequest(xacmlRequest), mapCustomResources(resources), mapResponse(xacmlResponse));
 	}
+
+	public void logAbacPermit(final XacmlRequest xacmlRequest, final XacmlResponse xacmlResponse, final Map<String, String> resources) {
+		log.info("{}{}; {}", mapRequest(xacmlRequest), mapCustomResources(resources), mapResponse(xacmlResponse));
+	}
+
+	private String mapCustomResources(final Map<String, String> resources) {
+		if (resources.isEmpty()) {
+			return "";
+		} else {
+			return ", " + resources.entrySet().stream().map(entry -> entry.getKey() + "=" + entry.getValue()).collect(Collectors.joining(", "));
+		}
+	}
+
+	private String mapRequest(final XacmlRequest xacmlRequest) {
+		return "Authorization Request: " +
+				xacmlRequest.getResources()
+						.stream()
+						.map(xacmlAttribute -> substringAfterLast(xacmlAttribute.getAttributeId(), SEPARATOR) + "=" + xacmlAttribute.getValue())
+						.collect(Collectors.joining(", "));
+	}
+
+	private String mapResponse(final XacmlResponse xacmlResponse) {
+		return "Authorization Response: " +
+				"decision=" + xacmlResponse.getDecision().getValue() +
+				(
+						xacmlResponse.getAdvices().isEmpty() ? "" :
+								", " + xacmlResponse.getAdvices()
+										.stream()
+										.flatMap(a -> a.getAttributeAssignments().stream())
+										.map(as -> substringAfterLast(as.getAttributeId(), SEPARATOR) + "=" + as.getValue())
+										.collect(Collectors.joining(", "))
+				);
+	}
+
+
 }
