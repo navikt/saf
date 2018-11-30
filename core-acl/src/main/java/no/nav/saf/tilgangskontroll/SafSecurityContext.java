17521f5c4fcf8b955dd85c7cdac3a4d99438c39e
Sigurd Midttun <sigurd.midttun@nav.no>
2018-11-30 12:38:41 +0100

    MMA-1602 La til saksbehandlerId i SafSecurityContext

--- core-acl/src/main/java/no/nav/saf/tilgangskontroll/SafSecurityContext.java
+++ core-acl/src/main/java/no/nav/saf/tilgangskontroll/SafSecurityContext.java
@@ -12,13 +12,15 @@ import no.nav.saf.exceptions.OidcAuthorizationException;
 public class SafSecurityContext {
 	private static final String OIDC_TOKEN_PREFIX = "Bearer ";
 	private final String oidcTokenBody;
+	private final String saksbehandlerId;
 
 	SafSecurityContext(String authorizationHeader) {
+		this.saksbehandlerId = getSubjectFromToken(authorizationHeader);
 		this.oidcTokenBody = getOidcTokenBody(authorizationHeader);
 	}
 
 	private String getOidcTokenBody(String authorizationHeader) {
-		if(authorizationHeader == null || !authorizationHeader.startsWith(OIDC_TOKEN_PREFIX)) {
+		if (isNotValidAuthorizationHeader(authorizationHeader)) {
 			return null;
 		}
 		try {
@@ -29,12 +31,37 @@ public class SafSecurityContext {
 		}
 	}
 
+	private String getSubjectFromToken(String authorizationHeader) {
+		if (isNotValidAuthorizationHeader(authorizationHeader)) {
+			return null;
+		}
+		try {
+			return JWT.decode(authorizationHeader.split(OIDC_TOKEN_PREFIX)[1]).getSubject();
+		} catch (JWTDecodeException e) {
+			log.error("Kunne ikke utlede subject fra OIDC-Token i header.", e);
+			return null;
+		}
+	}
+
 	public String getOidcTokenBody() {
-		if(oidcTokenBody == null) {
+		if (oidcTokenBody == null) {
 			throw new OidcAuthorizationException("Autentiseringsmekanisme er ikke støttet. " +
 					"Kun OIDC-token (JWT via OAuth 2.0) med header \"Authorization\" : \"Bearer {token}\" er tillatt.");
 		} else {
 			return oidcTokenBody;
 		}
+	}
+
+	public String getSaksbehandlerId() {
+		if (saksbehandlerId == null) {
+			throw new OidcAuthorizationException("Autentiseringsmekanisme er ikke støttet. " +
+					"Kun OIDC-token (JWT via OAuth 2.0) med header \"Authorization\" : \"Bearer {token}\" er tillatt.");
+		} else {
+			return saksbehandlerId;
+		}
+	}
+
+	private boolean isNotValidAuthorizationHeader(String authorizationHeader) {
+		return authorizationHeader == null || !authorizationHeader.startsWith(OIDC_TOKEN_PREFIX);
 	}
 }
