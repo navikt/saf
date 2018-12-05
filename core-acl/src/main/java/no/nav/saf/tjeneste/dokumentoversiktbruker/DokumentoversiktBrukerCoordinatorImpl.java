12c51a643ed0a58cceb3be626188232770b4fc40
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2018-12-05 10:52:38 +0100

    MMA-1062 Tilbake til tegnebrettet når det gjelder filtrering

--- core-acl/src/main/java/no/nav/saf/tjeneste/dokumentoversiktbruker/DokumentoversiktBrukerCoordinatorImpl.java
+++ core-acl/src/main/java/no/nav/saf/tjeneste/dokumentoversiktbruker/DokumentoversiktBrukerCoordinatorImpl.java
@@ -1,7 +1,5 @@
 package no.nav.saf.tjeneste.dokumentoversiktbruker;
 
-import io.reactivex.Flowable;
-import io.reactivex.schedulers.Schedulers;
 import no.nav.saf.domain.TilgangsmodellRepository;
 import no.nav.saf.domain.tilgangsmodell.TilgangBruker;
 import no.nav.saf.domain.tilgangsmodell.TilgangJournalpost;
@@ -57,14 +55,10 @@ public class DokumentoversiktBrukerCoordinatorImpl implements DokumentoversiktBr
 		}
 
 		final List<TilgangSak> tilgangSakList = tilgangsmodellRepository.findTilgangSaker(tilgangBruker, dokumentoversiktBrukerArguments.getTema(), safRequestContext);
-		List<TilgangSak> filteredTilgangSakList = Flowable.fromIterable(tilgangSakList)
-				.flatMap(tilgangSak ->
-						Flowable.just(tilgangSak)
-								.observeOn(Schedulers.io())
-								.filter(ts -> pep2.hasAccess(ts, safRequestContext))
-								.filter(ts -> pep3.hasAccess(ts, safRequestContext))
-				).toList()
-				.blockingGet();
+		List<TilgangSak> filteredTilgangSakList = tilgangSakList.stream()
+				.filter(ts -> pep2.hasAccess(ts, safRequestContext))
+				.filter(ts -> pep3.hasAccess(ts, safRequestContext))
+				.collect(Collectors.toList());
 
 		final List<TilgangJournalpost> tilgangJournalpostList = tilgangsmodellRepository.findTilgangJournalposter(
 				tilgangBruker,
@@ -76,13 +70,9 @@ public class DokumentoversiktBrukerCoordinatorImpl implements DokumentoversiktBr
 				safRequestContext
 		);
 
-		final List<TilgangJournalpost> filteredTilgangJournalpostList = Flowable.fromIterable(tilgangJournalpostList)
-				.flatMap(tilgangJournalpost ->
-						Flowable.just(tilgangJournalpost)
-								.observeOn(Schedulers.io())
-								.filter(tj -> pep4.hasAccess(tj, safRequestContext))
-				).toList()
-				.blockingGet();
+		final List<TilgangJournalpost> filteredTilgangJournalpostList = tilgangJournalpostList.stream()
+				.filter(tj -> pep4.hasAccess(tj, safRequestContext))
+				.collect(Collectors.toList());
 
 		return visningsmodellRepository.findJournalposter(filteredTilgangJournalpostList.stream().map(TilgangJournalpost::getJournalpostId).collect(Collectors.toList()), safRequestContext);
 	}
