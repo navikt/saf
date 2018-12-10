f08ec8bf0c4bfa3c360643cd1689f9f8d34ea43b
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2018-12-10 19:51:28 +0100

    MMA-1636 Satte max connections på connectionManagerFactory

--- app/src/main/java/no/nav/saf/ApplicationConfig.java
+++ app/src/main/java/no/nav/saf/ApplicationConfig.java
@@ -7,6 +7,7 @@ import no.nav.saf.metrics.DokMonitoringAspect;
 import no.nav.saf.platform.TomcatConfig;
 import org.apache.http.client.HttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -35,7 +36,12 @@ public class ApplicationConfig {
 
 	@Bean
 	HttpClient httpClient() {
-		return HttpClients.createDefault();
+		PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+		connectionManager.setMaxTotal(200);
+		connectionManager.setDefaultMaxPerRoute(200);
+		return HttpClients.custom()
+				.setConnectionManager(connectionManager)
+				.build();
 	}
 
 	@Bean
