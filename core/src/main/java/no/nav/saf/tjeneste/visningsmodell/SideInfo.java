9f57e5f135290dca121ed90ab427f1533e75ad50
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2019-01-04 08:21:44 +0100

    Fjernet totaltAntall fra SideInfo og API / metadata for å paginere bakover.

--- core/src/main/java/no/nav/saf/tjeneste/visningsmodell/SideInfo.java
+++ core/src/main/java/no/nav/saf/tjeneste/visningsmodell/SideInfo.java
@@ -9,10 +9,16 @@ import lombok.Value;
 public class SideInfo {
 	private static final SideInfo EMPTY_SIDE_INFO = new SideInfo(0,null, false, null, false);
 
+	// vi vil ikke eksponere denne med mindre konsumententene vil ha det
+	@Deprecated
 	private final int totaltAntall;
 	private final String sluttpeker;
 	private final boolean finnesNesteSide;
+	// det er ikke noe behov for å paginere bakover
+	@Deprecated
 	private final String startpeker;
+	// det er ikke noe behov for å paginere bakover
+	@Deprecated
 	private final boolean finnesForrigeSide;
 
 	public static SideInfo empty() {
