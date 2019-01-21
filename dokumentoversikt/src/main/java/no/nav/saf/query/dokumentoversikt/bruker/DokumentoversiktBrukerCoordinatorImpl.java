7a12e2b7cbcc6a9c287887a6d934c0a07498b363
Stian Låte <stian.late@nav.no>
2019-01-21 09:58:58 +0100

    MMA-2054 Justerte på dokumentoversikt metrikker

--- dokumentoversikt/src/main/java/no/nav/saf/query/dokumentoversikt/bruker/DokumentoversiktBrukerCoordinatorImpl.java
+++ dokumentoversikt/src/main/java/no/nav/saf/query/dokumentoversikt/bruker/DokumentoversiktBrukerCoordinatorImpl.java
@@ -60,7 +60,7 @@ class DokumentoversiktBrukerCoordinatorImpl implements DokumentoversiktBrukerCoo
 	}
 
 	@Override
-	@Monitor(value = "dok_request", extraTags = {"process", "dokumentOversikt", "BrukerEllerFagsak", "Bruker"}, histogram = true)
+	@Monitor(value = "dok_request", extraTags = {"process", "dokumentOversiktBruker"}, histogram = true)
 	public Dokumentoversikt hentDokumentoversikt(DokumentoversiktBrukerArguments dokumentoversiktBrukerArguments, SafRequestContext safRequestContext) {
 		TilgangBruker tilgangBruker = tilgangsmodellRepository.findTilgangBruker(dokumentoversiktBrukerArguments.getBrukerIdInput());
 		if (tilgangBruker != null) {
--- dokumentoversikt/src/main/java/no/nav/saf/query/dokumentoversikt/fagsak/DokumentoversiktFagsakCoordinatorImpl.java
+++ dokumentoversikt/src/main/java/no/nav/saf/query/dokumentoversikt/fagsak/DokumentoversiktFagsakCoordinatorImpl.java
@@ -59,7 +59,7 @@ class DokumentoversiktFagsakCoordinatorImpl implements DokumentoversiktFagsakCoo
 	}
 
 	@Override
-	@Monitor(value = "dok_request", extraTags = {"process", "dokumentOversikt", "BrukerEllerFagsak", "Fagsak"}, histogram = true)
+	@Monitor(value = "dok_request", extraTags = {"process", "dokumentOversiktBruker"}, histogram = true)
 	public Dokumentoversikt hentDokumentoversikt(DokumentoversiktFagsakArguments dokumentoversiktFagsakArguments, SafRequestContext safRequestContext) {
 		final FagsakIdInput fagsakIdInput = dokumentoversiktFagsakArguments.getFagsakIdInput();
 		final List<TilgangBruker> tilgangBrukerList = tilgangsmodellRepository.findTilgangBrukerList(fagsakIdInput);
