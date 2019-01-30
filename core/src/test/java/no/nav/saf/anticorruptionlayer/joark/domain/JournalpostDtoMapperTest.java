b5831008410fc1392fe8f312c704b1e75a688bcb
Sigurd Midttun <sigurd.midttun@nav.no>
2019-01-30 14:41:59 +0100

    MMA-2247 La til mappertest for filtrering på pep5Deny + fikset feilende tester

--- core/src/test/java/no/nav/saf/anticorruptionlayer/joark/domain/JournalpostDtoMapperTest.java
+++ core/src/test/java/no/nav/saf/anticorruptionlayer/joark/domain/JournalpostDtoMapperTest.java
@@ -1,14 +1,18 @@
 package no.nav.saf.anticorruptionlayer.joark.domain;
 
 import static no.nav.saf.cache.KeyGeneratorLocalCaching.getKeyForPep2dLocalCaching;
+import static no.nav.saf.cache.KeyGeneratorLocalCaching.getKeyForPep5LocalCaching;
 import static no.nav.saf.cache.KeyGeneratorLocalCaching.getKeyForPep6dLocalCaching;
 import static no.nav.saf.domain.DomainConstants.TILGANG_BRUKER;
 import static no.nav.saf.domain.kode.Kanal.SENTRAL_UTSKRIFT;
 import static org.assertj.core.groups.Tuple.tuple;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsCollectionContaining.hasItem;
 import static org.hamcrest.core.IsNot.not;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import no.nav.saf.anticorruptionlayer.joark.domain.kode.FagomradeCode;
 import no.nav.saf.anticorruptionlayer.joark.domain.kode.FagsystemCode;
@@ -51,9 +55,6 @@ import java.util.List;
  */
 @ExtendWith(MockitoExtension.class)
 class JournalpostDtoMapperTest {
-
-	private static final String OIDC_TOKEN_PERSON = "Bearer " + "eyAidHlwIjogIkpXVCIsICJraWQiOiAiU0gxSWVSU2sxT1VGSDNzd1orRXVVcTE5VHZRPSIsICJhbGciOiAiUlMyNTYiIH0.eyAiYXRfaGFzaCI6ICJvNFUwMVhKNmlnRmw0VGYwdFRkYjR3IiwgInN1YiI6ICJaOTkwNDI0IiwgImF1ZGl0VHJhY2tpbmdJZCI6ICJlYTdmNWUxMi1jYjZjLTQ1ZjUtYmViMi0wYjVkYmI5ZDQ3YTItMTMzNzkzNCIsICJpc3MiOiAiaHR0cHM6Ly9pc3NvLXQuYWRlby5ubzo0NDMvaXNzby9vYXV0aDIiLCAidG9rZW5OYW1lIjogImlkX3Rva2VuIiwgImF1ZCI6ICJpZGEtdCIsICJjX2hhc2giOiAiRnJwNzhwdlJZU0VPMExjUktPUFdWdyIsICJvcmcuZm9yZ2Vyb2NrLm9wZW5pZGNvbm5lY3Qub3BzIjogIjJjYjQ2OGU4LThmMjItNGY1NS1hYTQ4LWM1NWExYjA4YmQ1ZiIsICJhenAiOiAiaWRhLXQiLCAiYXV0aF90aW1lIjogMTU0MzU3Nzk3MiwgInJlYWxtIjogIi8iLCAiZXhwIjogMTU0MzU4MTU3MiwgInRva2VuVHlwZSI6ICJKV1RUb2tlbiIsICJpYXQiOiAxNTQzNTc3OTcyIH0.NRgKaZhZ7qbBbJMUj_l9kzGOv7yOJVRVZDqmK0-G9lxzZs4jW1AtvFWqJRO9dd_djlIOGXz93UnuMNpWYWuoUd_S9gVc53yUjquzrys1IK8Zjd89smEl_9QP3ya8z7ISv48DciJORxdB2XT8rr2qpltYjKrCE2QmmK2ctAhy9QuFwEoZnctrR8IDKhUJCGd8LXPXddNRNEDL4-A47KwkF0UcfoDzPXznyZ2cbV4IkT3zvGqqwO3hovdrpadBdf204hClcmETYN3frRh1qHuTUqrBL7ualfqs-eDa4FKd77Mwu02LqPQGVpt8Ebebtv3OlS28YDchx8ng_P05okSjZg";
-
 	private static final String DOKUMENT_INFO_ID = "1234";
 	private static final VariantFormatCode VARIANT_FORMAT_CODE_ARKIV = VariantFormatCode.ARKIV;
 	private static final VariantFormatCode VARIANT_FORMAT_CODE_SLADDET = VariantFormatCode.SLADDET;
@@ -86,9 +87,10 @@ class JournalpostDtoMapperTest {
 
 	@Test
 	void shouldMapJournalpostDtoWithUtgaaendeJournalpost() {
-
 		JournalpostDto journalpostDto = buildJournalpostDtoUtgaaendeType(JournalStatusCode.E);
 		RequestCache requestCache = createArkivsakCacheRequestCache();
+		String tilgangKeyPep5LocalCaching = getKeyForPep5LocalCaching(String.valueOf(JOURNALPOST_ID), DOKUMENT_INFO_ID);
+		requestCache.putObject(tilgangKeyPep5LocalCaching, true);
 
 		Journalpost journalpost = mapper.mapJournalpostDto(journalpostDto, requestCache);
 
@@ -108,9 +110,10 @@ class JournalpostDtoMapperTest {
 
 	@Test
 	void shouldMapJournalpostDtoWithInngaaendeJournalpost() {
-
 		JournalpostDto journalpostDto = buildJournalpostDtoInngaaendeType();
 		RequestCache requestCache = createTilgangBrukerRequestCache();
+		String tilgangKeyPep5LocalCaching = getKeyForPep5LocalCaching(String.valueOf(JOURNALPOST_ID), DOKUMENT_INFO_ID);
+		requestCache.putObject(tilgangKeyPep5LocalCaching, true);
 
 		Journalpost journalpost = mapper.mapJournalpostDto(journalpostDto, requestCache);
 
@@ -126,7 +129,20 @@ class JournalpostDtoMapperTest {
 		assertThat(journalpost.getRelevanteDatoer(), not(hasItem(new RelevantDato(EKSPEDERT_DATO, Datotype.DATO_EKSPEDERT))));
 		assertThat(journalpost.getRelevanteDatoer(), hasItem(new RelevantDato(MOTTAT_DATO, Datotype.DATO_REGISTRERT)));
 		assertThat(journalpost.getRelevanteDatoer(), hasItem(new RelevantDato(JOURNAL_DATO, Datotype.DATO_JOURNALFOERT)));
+	}
 
+	@Test
+	void shouldNotMapDokumentinfoIngenTilgangPep5() {
+		JournalpostDto journalpostDto = buildJournalpostDtoInngaaendeType();
+		RequestCache requestCache = createTilgangBrukerRequestCache();
+		String tilgangKeyPep5LocalCaching = getKeyForPep5LocalCaching(String.valueOf(JOURNALPOST_ID), DOKUMENT_INFO_ID);
+		requestCache.putObject(tilgangKeyPep5LocalCaching, false);
+
+		Journalpost journalpost = mapper.mapJournalpostDto(journalpostDto, requestCache);
+
+		assertThat(journalpost, is(notNullValue()));
+		assertThat(journalpost.getJournalpostId(), is(String.valueOf(JOURNALPOST_ID)));
+		assertTrue(journalpost.getDokumenter().isEmpty());
 	}
 
 	@Test
@@ -187,6 +203,8 @@ class JournalpostDtoMapperTest {
 
 		String tilgangKeyPep2dLocalCaching = getKeyForPep2dLocalCaching(journalpostDto.getFagomrade().name());
 
+		String tilgangKeyPep5LocalCaching = getKeyForPep5LocalCaching(String.valueOf(JOURNALPOST_ID), DOKUMENT_INFO_ID);
+
 		String tilgangKeyPep6dLocalCachingVariantArkiv = getKeyForPep6dLocalCaching(
 				String.valueOf(JOURNALPOST_ID), DOKUMENT_INFO_ID, VARIANT_FORMAT_CODE_ARKIV.getSafVariantformat()
 						.name(), SKJERMING_TYPE_CODE_POL.getSafSkjerming().name());
@@ -198,6 +216,7 @@ class JournalpostDtoMapperTest {
 
 		RequestCache requestCache = new RequestCache();
 		requestCache.putObject(tilgangKeyPep2dLocalCaching, Boolean.TRUE);
+		requestCache.putObject(tilgangKeyPep5LocalCaching, Boolean.TRUE);
 		requestCache.putObject(tilgangKeyPep6dLocalCachingVariantArkiv, Boolean.TRUE);
 		requestCache.putObject(tilgangKeyPep6dLocalCachingVariantSladdet, Boolean.FALSE);
 
