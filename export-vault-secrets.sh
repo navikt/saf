93d7b1736352f4d471bc7eace2d53afa054a558f
Heidi Elisabeth Sando <heidi.elisabeth.sando@nav.no>
2020-01-30 12:18:16 +0100

    Fjernet unødvendig kode i init-script.

--- export-vault-secrets.sh
+++ export-vault-secrets.sh
@@ -10,14 +10,3 @@ then
     echo "Setting serviceuser_password"
     export  serviceuser_***passord=gammelt_passord***)
 fi
-if test -d /var/run/secrets/nais.io/vault;
-then
-    echo "Setting no_nav_security_jwt_issuer_azurev1_acceptedAudience"
-    export  no_nav_security_jwt_issuer_azurev1_acceptedAudience=$(cat /var/run/secrets/nais.io/vault/no_nav_security_jwt_issuer_azurev1_acceptedAudience)
-    echo "Setting no_nav_security_jwt_issuer_azurev2_acceptedAudience"
-    export  no_nav_security_jwt_issuer_azurev2_acceptedAudience=$(cat /var/run/secrets/nais.io/vault/no_nav_security_jwt_issuer_azurev2_acceptedAudience)
-    echo "Setting no_nav_security_jwt_issuer_openam_acceptedAudience"
-    export  no_nav_security_jwt_issuer_openam_acceptedAudience=$(cat /var/run/secrets/nais.io/vault/no_nav_security_jwt_issuer_openam_acceptedAudience)
-    echo "Setting no_nav_security_jwt_issuer_reststs_acceptedAudience"
-    export  no_nav_security_jwt_issuer_reststs_acceptedAudience=$(cat /var/run/secrets/nais.io/vault/no_nav_security_jwt_issuer_reststs_acceptedAudience)
-fi
