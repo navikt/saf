764b9423d652047f164e620008d3600a236eb304
Kim Andre Pollvik <kim.pollvik@nav.no>
2020-09-16 12:17:34 +0200

    MMA-4764: fikset linjeskift

--- app/src/main/java/no/nav/saf/swagger/SwaggerConfig.java
+++ app/src/main/java/no/nav/saf/swagger/SwaggerConfig.java
@@ -92,8 +92,7 @@ public class SwaggerConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.
-                addResourceHandler("/swagger-ui/**")
+        registry.addResourceHandler("/swagger-ui/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/")
                 .resourceChain(false);
     }
