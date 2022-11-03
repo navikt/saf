41bf4f0da0d8e18b622ac75ebd8228a84e881ef0
Christian Strandenæs <Christian.Fredrik.Osterby.Strandenes@nav.no>
2022-11-03 17:25:16 +0100

    MMA-6443 fjern ubrukt exception, mer informativ tekst når abac nekter tilgang på bakgrunn av tema

--- core/src/main/java/no/nav/saf/tilgangskontroll/pep/DenyReasons.java
+++ core/src/main/java/no/nav/saf/tilgangskontroll/pep/DenyReasons.java
@@ -7,12 +7,15 @@ package no.nav.saf.tilgangskontroll.pep;
  */
 public final class DenyReasons {
 	public static final String DENY_PREFIX = "Tilgang til ressurs ble avvist. ";
+	public static final String CONTACT_US_SUFFIX = " Hvis ikke dette fungerer, kontakt oss på #team_dokumentløsninger";
 	public static final String PEP1G_DENY_REASON = DENY_PREFIX +
 			"Saksbehandler eller system har ikke tilgang til ressurs tilhørende bruker som har kode 6/7, egen ansatt eller utenfor tillatt geografisk område.";
 	public static final String PEP2_DENY_REASON = DENY_PREFIX +
 			"Saksbehandler eller system har ikke tilgang til tema ressurset tilhører eller på grunn av Forvaltningsloven § 19.";
 	public static final String PEP2D_DENY_REASON = DENY_PREFIX +
-			"Saksbehandler eller system har ikke tilgang til tema ressurset tilhører eller geografisk område.";
+			"Saksbehandler eller system har ikke tilgang til tema ressurset tilhører eller geografisk område. " +
+			"Sjekk om bruker eller serviceuser har riktig gruppetilgang for tema. Det kan også skyldes at tema ikke er konfigurert i ABAC." +
+			CONTACT_US_SUFFIX;
 	public static final String PEP3_DENY_REASON = DENY_PREFIX +
 			"Saksbehandler eller system har ikke tilgang til ressurs der en av partene i bidragssaken har kode6/7 eller egen ansatt.";
 	public static final String PEP4_DENY_REASON = DENY_PREFIX +
