2383d366180e6c214ddd52d132a3d6dffaa4ab29
Anders Opskar Voldsund <anders.voldsund@nav.no>
2023-04-18 14:01:49 +0200

    Test hindring av PR grunna code scanning

--- core/src/main/java/no/nav/saf/anticorruptionlayer/bisys/BisysAntiCorruptionLayer.java
+++ core/src/main/java/no/nav/saf/anticorruptionlayer/bisys/BisysAntiCorruptionLayer.java
@@ -3,9 +3,6 @@ package no.nav.saf.anticorruptionlayer.bisys;
 import no.nav.saf.domain.Arkivsak;
 import no.nav.saf.domain.BidragSak;
 
-/**
- * @author Joakim Bjørnstad, Jbit AS
- */
 public interface BisysAntiCorruptionLayer {
 	BidragSak hentBidragSakByArkivsak(Arkivsak arkivsak);
 }
