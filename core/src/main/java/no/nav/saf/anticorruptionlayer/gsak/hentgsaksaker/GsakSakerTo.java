03730c2b9cbe0a2bd38a79ebae4dbb820a80aff6
Kristian Laskemoen <kristian.laskemoen@nav.no>
2023-11-15 10:24:44 +0100

    Revert "MMA-7162 rettet testen"
    This reverts commit fbe30ae5002a8177962d47bae35a642ddca1a3c0.

--- core/src/main/java/no/nav/saf/anticorruptionlayer/gsak/hentgsaksaker/GsakSakerTo.java
+++ core/src/main/java/no/nav/saf/anticorruptionlayer/gsak/hentgsaksaker/GsakSakerTo.java
@@ -6,7 +6,6 @@ import lombok.Builder;
 import lombok.Value;
 
 import java.time.OffsetDateTime;
-import java.time.ZonedDateTime;
 
 @JsonDeserialize(builder = GsakSakerTo.GsakSakerToBuilder.class)
 @Value
@@ -19,7 +18,7 @@ public class GsakSakerTo {
 	private final String orgnr;
 	private final String fagsakNr;
 	private final String opprettetAv;
-	private final ZonedDateTime opprettetTidspunkt;
+	private final OffsetDateTime opprettetTidspunkt;
 
 	@JsonPOJOBuilder(withPrefix = "")
 	public static class GsakSakerToBuilder {
