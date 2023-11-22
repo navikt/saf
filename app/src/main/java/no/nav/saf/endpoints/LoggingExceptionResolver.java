fe0aa6aec9f1b62ae4fbbcbba5bb3250cba5c6cc
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2023-11-22 12:02:38 +0100

    MMA-6455 Logger requestURI for ukjente tekniske feil

--- app/src/main/java/no/nav/saf/endpoints/LoggingExceptionResolver.java
+++ app/src/main/java/no/nav/saf/endpoints/LoggingExceptionResolver.java
@@ -1,24 +1,26 @@
 package no.nav.saf.endpoints;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import no.nav.saf.exceptions.SafFunctionalException;
+import no.nav.saf.exceptions.SafTechnicalException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.AbstractHandlerExceptionResolver;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class LoggingExceptionResolver extends AbstractHandlerExceptionResolver {
 	@Override
 	protected ModelAndView doResolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
-		if(ex instanceof SafFunctionalException) {
-			if(logger.isWarnEnabled()) {
+		if (ex instanceof SafFunctionalException) {
+			if (logger.isWarnEnabled()) {
 				logger.warn(ex.getMessage(), ex);
 			}
+		} else if(ex instanceof SafTechnicalException) {
+			logger.error(ex.getMessage(), ex);
 		} else {
-			if(logger.isErrorEnabled()) {
-				logger.error(ex.getMessage(), ex);
+			if (logger.isErrorEnabled()) {
+				logger.error("requestURI=" + request.getRequestURI() + "\n" + ex.getMessage(), ex);
 			}
 		}
 		return null;
