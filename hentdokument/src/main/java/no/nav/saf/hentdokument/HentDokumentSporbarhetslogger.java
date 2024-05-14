ea23e64bf2dae61fd23573af236f3501d7c8c967
Christian Strandenæs <Christian.Fredrik.Osterby.Strandenes@nav.no>
2024-05-14 16:53:42 +0200

    MMA-7393 unngå nullpointerexceptions i auditlog i hentdokument

--- hentdokument/src/main/java/no/nav/saf/hentdokument/HentDokumentSporbarhetslogger.java
+++ hentdokument/src/main/java/no/nav/saf/hentdokument/HentDokumentSporbarhetslogger.java
@@ -8,6 +8,9 @@ import no.nav.saf.domain.kode.Tema;
 import no.nav.saf.domain.tilgangsmodell.TilgangBruker;
 import no.nav.saf.domain.tilgangsmodell.TilgangSak;
 import no.nav.saf.tilgangskontroll.SafRequestContext;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.Instant;
 
@@ -17,8 +20,10 @@ import static no.nav.common.audit_log.cef.AuthorizationDecision.PERMIT;
  * Sporingslogger til nais.audit.
  * https://confluence.adeo.no/display/BOA/saf+-+Sporingslogg+hentdokument
  */
-@Slf4j(topic = "hentdokument_sporbarhetslogg")
+@Slf4j
 class HentDokumentSporbarhetslogger {
+	private static final Logger auditlog = LoggerFactory.getLogger("hentdokument_sporbarhetslogg");
+
 	private static final Tema UKJENT_TEMA = Tema.UKJ;
 	private static final String UKJENT_BRUKERID = "ukjent";
 	private static final String MACHINE_HENT_DOKUMENT_SYSTEM = "hentdokument_system";
@@ -28,29 +33,33 @@ class HentDokumentSporbarhetslogger {
 
 	void logPermit(String journalpostId, String dokumentInfoId, String variantFormat,
 				   HentDokumentTilgang hentDokumentTilgang, SafRequestContext safRequestContext) {
-		String journalpostTittel = hentDokumentTilgang.tilgangJournalpost().getJournalpostTittel();
-		String sourceUserId = safRequestContext.getUserId();
-		CefMessage message = CefMessage.builder()
-				.version(0)
-				.applicationName("joark")
-				.loggerName("saf_hentdokument")
-				.logFormatVersion("1.0")
-				.event(CefMessageEvent.ACCESS)
-				.severity(CefMessageSeverity.INFO)
-				.timeEnded(Instant.now().toEpochMilli())
-				.callId(safRequestContext.getNavCallId())
-				.name(humanReadableAction(safRequestContext))
-				.destinationUserId(getBrukerId(hentDokumentTilgang.tilgangBruker()))
-				.sourceUserId(sourceUserId)
-				.authorizationDecision(PERMIT)
-				.extension("act", machineReadableAction(safRequestContext))
-				.flexString(1, "journalpostId", journalpostId)
-				.flexString(2, "dokumentInfoId", dokumentInfoId)
-				.customString(3, "variantformat", variantFormat)
-				.customString(5, "tittel", journalpostTittel != null ? journalpostTittel : "ukjent tittel")
-				.customString(6, "tema", getTema(hentDokumentTilgang.tilgangSak()))
-				.build();
-		log.info(message.toString());
+		try {
+			String journalpostTittel = hentDokumentTilgang.tilgangJournalpost().getJournalpostTittel();
+			String sourceUserId = safRequestContext.getUserId();
+			CefMessage message = CefMessage.builder()
+					.version(0)
+					.applicationName("joark")
+					.loggerName("saf_hentdokument")
+					.logFormatVersion("1.0")
+					.event(CefMessageEvent.ACCESS)
+					.severity(CefMessageSeverity.INFO)
+					.timeEnded(Instant.now().toEpochMilli())
+					.callId(safRequestContext.getNavCallId())
+					.name(humanReadableAction(safRequestContext))
+					.destinationUserId(getBrukerId(hentDokumentTilgang.tilgangBruker()))
+					.sourceUserId(sourceUserId != null ? sourceUserId : "Ukjent Bruker")
+					.authorizationDecision(PERMIT)
+					.extension("act", machineReadableAction(safRequestContext))
+					.flexString(1, "journalpostId", journalpostId)
+					.flexString(2, "dokumentInfoId", dokumentInfoId)
+					.customString(3, "variantformat", variantFormat)
+					.customString(5, "tittel", journalpostTittel != null ? journalpostTittel : "ukjent tittel")
+					.customString(6, "tema", getTema(hentDokumentTilgang.tilgangSak()))
+					.build();
+			auditlog.info(message.toString());
+		} catch (NullPointerException e) {
+			log.error("saf hentdokument Unable to audit log for journalpostId={} dokumentInfoId={}, variantFormat={}", journalpostId, dokumentInfoId, variantFormat, e);
+		}
 	}
 
 	private String humanReadableAction(SafRequestContext safRequestContext) {
@@ -70,13 +79,13 @@ class HentDokumentSporbarhetslogger {
 	}
 
 	private String getBrukerId(TilgangBruker tilgangBruker) {
-		if (tilgangBruker == null) {
-			return UKJENT_BRUKERID;
+		if (tilgangBruker != null) {
+			if (tilgangBruker.isPerson() && tilgangBruker.getFoedselsnr() != null) {
+				return tilgangBruker.getFoedselsnr();
+			} else if (tilgangBruker.isOrganisasjon()) {
+				return tilgangBruker.getOrgnummer();
+			}
 		}
-		if (tilgangBruker.isPerson()) {
-			return tilgangBruker.getFoedselsnr();
-		} else {
-			return tilgangBruker.getOrgnummer();
-		}
+		return UKJENT_BRUKERID;
 	}
 }
