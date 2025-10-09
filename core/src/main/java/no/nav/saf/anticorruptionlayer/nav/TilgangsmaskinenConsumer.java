73c30024f3a47c48af7ae3ab4efb6726ffe9449d
Christian Strandenæs <Christian.Fredrik.Osterby.Strandenes@nav.no>
2025-10-09 16:49:05 +0200

    MMA-8339 Hent NAVIdent fra eksisterende SafRequestContext-stack
    ... og ikke fra et sted den ikke finnes i tokenet 😅

--- core/src/main/java/no/nav/saf/anticorruptionlayer/nav/TilgangsmaskinenConsumer.java
+++ core/src/main/java/no/nav/saf/anticorruptionlayer/nav/TilgangsmaskinenConsumer.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import no.nav.saf.config.SafProperties;
 import no.nav.saf.exceptions.SafFunctionalException;
 import no.nav.saf.exceptions.SafTechnicalException;
+import no.nav.saf.tilgangskontroll.SafRequestContext;
 import no.nav.saf.tilgangskontroll.pep.PepAnswer;
 import no.nav.saf.tilgangskontroll.pep.reasons.EgenAnsattReason;
 import no.nav.saf.tilgangskontroll.pep.reasons.FortroligAdresseReason;
@@ -15,7 +16,6 @@ import no.nav.saf.tilgangskontroll.pep.reasons.PersonUtlandReason;
 import no.nav.saf.tilgangskontroll.pep.reasons.StrengtFortroligAdresseReason;
 import no.nav.saf.tilgangskontroll.pep.reasons.StrengtFortroligAdresseUtlandReason;
 import no.nav.saf.tilgangskontroll.pep.reasons.UkjentEllerTekniskReason;
-import no.nav.security.token.support.core.jwt.JwtToken;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
@@ -43,15 +43,20 @@ public class TilgangsmaskinenConsumer {
 
 	@Retry(name = TILGANGSMASKINEN_INSTANCE)
 	@CircuitBreaker(name = TILGANGSMASKINEN_INSTANCE)
-	public PepAnswer navIdentHasAccess(String identifikator, JwtToken entraIdToken) {
+	public PepAnswer navIdentHasAccess(String identifikator, SafRequestContext safRequestContext) {
 		try {
-			return texasAuthorizedRestClient.post()
-					.uri(uriBuilder -> uriBuilder.path("/api/v1/ccf/komplett/" + entraIdToken.getSubject()).build())
-					.attributes(attributes -> {
-						attributes.put(TARGET_SCOPE, safProperties.getEndpoints().getTilgangsmaskinen().getScope());
-					})
-					.body(identifikator)
-					.exchange((request, response) -> handleResponseFromTilgangsmaskinen(response));
+			if (safRequestContext.isUserIdNavAnsatt()) {
+				return texasAuthorizedRestClient.post()
+						.uri(uriBuilder -> uriBuilder.path("/api/v1/ccf/komplett/" + safRequestContext.getUserId()).build())
+						.attributes(attributes -> {
+							attributes.put(TARGET_SCOPE, safProperties.getEndpoints().getTilgangsmaskinen().getScope());
+						})
+						.body(identifikator)
+						.exchange((request, response) -> handleResponseFromTilgangsmaskinen(response));
+			} else {
+				log.error("Kunne ikke gjøre kall mot tilgangsmaskinen fordi userId ikke er en NAV-ident");
+				throw new SafFunctionalException("Kunne ikke gjøre kall mot tilgangsmaskinen fordi brukers ident ikke er en NAV-ident");
+			}
 		} catch (Exception e) {
 			log.error("Kall mot Tilgangsmaskinen feilet med en ukjent teknisk feil. message={}", e.getMessage(), e);
 			throw e;
